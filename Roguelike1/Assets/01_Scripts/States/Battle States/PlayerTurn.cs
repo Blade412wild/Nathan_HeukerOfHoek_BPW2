cdbc5b70331cb5308173bf95ee0773c602eb2a4a
nathan <nathanhoh951@gmail.com>
2023-03-29 17:57:22 +0200

    Added OnMouseClick event

--- Roguelike1/Assets/01_Scripts/States/Battle States/PlayerTurn.cs
+++ Roguelike1/Assets/01_Scripts/States/Battle States/PlayerTurn.cs
@@ -5,38 +5,52 @@ using UnityEngine;
 
 public class PlayerTurn : BaseState
 {
+    public int EnergyRefillAmount;
+
+    [SerializeField]
+    private GameEvent MouseClicked;
+
     private BattleHUD playerHUD;
     private Unit PlayerUnit;
     private Player player;
     private PlayerInputAction playerInputAction;
 
 
+
+
     public override void OnEnter()
     {
         Debug.Log("PlayerTurn");
+        PlayerUnit.CurrentEnergy = PlayerUnit.CurrentEnergy + EnergyRefillAmount;
+        playerInputAction.FreeRoam.Click.performed += ctx => { ClickedMouse(); };
     }
 
     public override void OnExit()
     {
-
+        playerInputAction.FreeRoam.Click.performed -= ctx => { ClickedMouse(); };
     }
 
     public override void OnUpdate()
     {
-        if(PlayerUnit.CurrentEnergy == 0)
-        {
-            
-        }
+
     }
 
     public void CollectData()
     {
-        //player data
+        playerInputAction = new PlayerInputAction();
+        playerInputAction.FreeRoam.Enable();
+
         player = FindAnyObjectByType<Player>();
         PlayerUnit = player.GetComponent<Unit>();
         //playerHUD = FindAnyObjectByType<BattleHUD>();
         //playerHUD.SetHUD(PlayerUnit);
 
         Debug.Log("collected Data");
+    }
+
+    public void ClickedMouse()
+    {
+        Debug.Log("Clicked Mouse");
+        MouseClicked?.Invoke();
     }
 }
---
+++ Roguelike1/Assets/01_Scripts/prototype/MouseClicked.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClicked : MonoBehaviour
+{
+
+    public void CheckIfMouseClickIsEnemy()
+    {
+        Debug.Log("Check if hit is target");
+    }
+
+    private void DoDamage()
+    {
+
+    }
+
+
+
+}
